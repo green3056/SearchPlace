2fdb2d1e60324c4c2c730223495e0d5b0e515f6e
green3056 <goodev@kakao.com>
2022-08-16 20:09:30 +0900

    (setting): 그레이들 Springboot 웹 프로젝트 생성
    [How]
    - Spring initializr 사용하여 생성함 (https://start.spring.io/)

---
+++ src/main/java/com/green/SearchPlace/SearchPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.green.SearchPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchPlaceApplication.class, args);
+	}
+
+}
